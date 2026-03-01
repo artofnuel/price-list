afe10bdd2649b7aee4f1bcdbd0523e7e1e86bcbe
Emmanuel Inua <artofnuel@gmail.com>
2026-03-01 12:18:53 +0100

    Update url.js

--- src/lib/utils/url.js
+++ src/lib/utils/url.js
@@ -1,4 +1,6 @@
 export const getBaseUrl = () => {
+  if (typeof window !== 'undefined') return window.location.origin
+
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel
